c1c048fe3edeb2540b7e7a37bdb91e72437c43aa
varun77x <varun77x@gmail.com>
2025-04-16 01:19:15 +0530

    removed base

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -1,18 +1,14 @@
-import bgHero from '../assets/bgHero.jpg';
-export default function Hero(){
-    return (
-        <>
-        {/* Hero Section (Company Name with background) */}
-              {/* <section className="h-screen bg-gradient-to-r from-blue-500 to-teal-500 text-white flex justify-center items-center">
-                <h1 className="text-7xl font-bold">XANGAM</h1>
-              </section> */}
-              <section className="h-screen bg-cover bg-center text-white flex justify-center items-center" 
-               style={{
-                backgroundImage: `url(${bgHero})`,
-              }}
-              >
-                <h1 className="text-7xl font-bold">XANGAM</h1>
-              </section>
-        </>
-    )
+
+
+export default function Hero() {
+  return (
+    <>
+      {/* Hero Section (Company Name with background) */}
+      <section
+        className="h-screen bg-black text-white flex justify-center items-center"
+      >
+        <h1 className="text-7xl font-bold">XANGAM</h1>
+      </section>
+    </>
+  );
 }
