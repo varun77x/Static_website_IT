88c69253387ac8a7a0fef023117d1ee0de17503b
varun77x <varun77x@gmail.com>
2025-04-17 13:04:21 +0530

    minor changes

--- frontend/src/Components/Partners.jsx
+++ frontend/src/Components/Partners.jsx
@@ -3,7 +3,7 @@ export default function Partners() {
   return (
     <section
       id="partners"
-      className="w-full bg-white text-center flex flex-col justify-center items-center px-4 sm:px-6 py-4 mt-20"
+      className="w-full bg-white text-center flex flex-col justify-center items-center px-4 sm:px-6 py-4 sm:py-6 mt-10"
     >
       <h2 className="text-4xl sm:text-5xl lg:text-6xl font-semibold mb-6">
         PARTNERS
